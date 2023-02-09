c17cfb4f7e4b4db854a1b447d61445fb9c1b767e
Crunchpoint <87291392+Crunchpoint@users.noreply.github.com>
2023-02-09 14:00:06 +0900

    server api key update

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const axios = require("axios");
 const cors = require("cors");
 const app = express();
 const port = 8080;
+require("dotenv").config();
+
+const apiKey = process.env.REACT_APP_API_KEY;
 
 app.use(cors());
 
-const dataUrl = `http://openapi.seoul.go.kr:8088/54476354466d796338306f6b524278/json/culturalEventInfo/1/1000/`;
-const dataUrl2 = `http://openapi.seoul.go.kr:8088/54476354466d796338306f6b524278/json/culturalEventInfo/1001/2000/`;
-const dataUrl3 = `http://openapi.seoul.go.kr:8088/54476354466d796338306f6b524278/json/culturalEventInfo/2001/3000/`;
-const dataUrl4 = `http://openapi.seoul.go.kr:8088/54476354466d796338306f6b524278/json/culturalEventInfo/3001/4000/`;
-const dataUrl5 = `http://openapi.seoul.go.kr:8088/54476354466d796338306f6b524278/json/culturalSpaceInfo/1/1000/`;
+const dataUrl = `http://openapi.seoul.go.kr:8088/${apiKey}/json/culturalEventInfo/1/1000/`;
+const dataUrl2 = `http://openapi.seoul.go.kr:8088/${apiKey}/json/culturalEventInfo/1001/2000/`;
+const dataUrl3 = `http://openapi.seoul.go.kr:8088/${apiKey}/json/culturalEventInfo/2001/3000/`;
+const dataUrl4 = `http://openapi.seoul.go.kr:8088/${apiKey}/json/culturalEventInfo/3001/4000/`;
+const dataUrl5 = `http://openapi.seoul.go.kr:8088/${apiKey}/json/culturalSpaceInfo/1/1000/`;
 
 app.get("/data", (req, res) => {
   axios
